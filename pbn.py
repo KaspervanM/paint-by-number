25266c214bca76dfdd8dbbc5409e3b33b91a9b85
Kasper van Maasdam <kvanmaasdam.tech@bakkies.eu>
2025-12-29 20:35:07 +0100

    feat: Floyd-Steinberg error diffusion dithering.

--- pbn.py
+++ pbn.py
@@ -38,6 +38,40 @@ class NearestColorQuantization:
         return math.sqrt(sum((ac - bc) ** 2 for ac, bc in zip(a, b)))
 
 
+class FloydSteinbergDithering:
+    """Color quantization using Floyd–Steinberg error diffusion dithering."""
+
+    def quantize(self, image: Image.Image, palette: List[Color]) -> Image.Image:
+        """Quantize an image to the palette using Floyd–Steinberg dithering."""
+        image = image.convert("RGB")
+        pixels = image.load()
+        width, height = image.size
+
+        buffer = [[list(pixels[x, y]) for x in range(width)] for y in range(height)]
+
+        for y in range(height):
+            for x in range(width):
+                old_pixel = buffer[y][x]
+                new_pixel = self._nearest_color(tuple(int(c) for c in old_pixel), palette)
+                pixels[x, y] = new_pixel
+                error = [old_pixel[i] - new_pixel[i] for i in range(3)]
+
+                if x + 1 < width:
+                    buffer[y][x + 1] = [buffer[y][x + 1][i] + error[i] * 7 / 16 for i in range(3)]
+                if x - 1 >= 0 and y + 1 < height:
+                    buffer[y + 1][x - 1] = [buffer[y + 1][x - 1][i] + error[i] * 3 / 16 for i in range(3)]
+                if y + 1 < height:
+                    buffer[y + 1][x] = [buffer[y + 1][x][i] + error[i] * 5 / 16 for i in range(3)]
+                if x + 1 < width and y + 1 < height:
+                    buffer[y + 1][x + 1] = [buffer[y + 1][x + 1][i] + error[i] * 1 / 16 for i in range(3)]
+
+        return image
+
+    def _nearest_color(self, color: Color, palette: List[Color]) -> Color:
+        """Return the palette color with minimal Euclidean distance."""
+        return min(palette, key=lambda p: math.sqrt(sum((pc - c) ** 2 for pc, c in zip(p, color))))
+
+
 class PaintByNumber:
     """Paint-by-number image processor."""
 
@@ -67,7 +101,7 @@ def main(image_path: pathlib.Path, palette_path: pathlib.Path) -> None:
     """Load an image and palette, apply paint-by-number quantization, and display it."""
     image = Image.open(image_path)
     palette = load_palette(palette_path)
-    algorithm = NearestColorQuantization()
+    algorithm = FloydSteinbergDithering()
     pbn = PaintByNumber(palette, algorithm)
     result = pbn.process(image)
     result.show()
