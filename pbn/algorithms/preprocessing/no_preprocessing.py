14741abba95e313fabf801a87d002fa811c4ef46
Kasper van Maasdam <kvanmaasdam.tech@bakkies.eu>
2025-12-30 16:16:38 +0100

    style: remove unused imports

--- pbn/algorithms/preprocessing/no_preprocessing.py
+++ pbn/algorithms/preprocessing/no_preprocessing.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, cast, Tuple
+from typing import List, Optional
 from PIL import Image
 
 from .base import ImageProcessingAlgorithm
--- pbn/core.py
+++ pbn/core.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 from PIL import Image
 
 from pbn.algorithms.preprocessing import ImageProcessingAlgorithm
